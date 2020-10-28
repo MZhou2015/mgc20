0c2c25d94ea02866c5ecc28fbc2832d3e4064586
Michael <ming01zhou@gmail.com>
2020-10-27 20:05:25 -0700

    proEdit

--- src/php/product/product_edit.php
+++ src/php/product/product_edit.php
@@ -5,12 +5,19 @@ require('../docs/fn_php_2020march.php');
 /****************************************/
 $act = "main" ;
 if(isset($_GET['act'])) $acc  = $_GET['act']  ;
+if($act == "POST") {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    foreach ($_POST as $val) {
+    echo "$val <br> "; 
+      }  
+  $res['post'] =$val ;
+ }
 switch ($act) {
     case "main":
-      echo "Your favorite color is red!";
+      echo "This is your Main stuff!";
       break;
-    case "blue":
-      echo "Your favorite color is blue!";
+    case "list":
+      echo "Your list Here!";
       break;
     case "green":
       echo "Your favorite color is green!";
@@ -18,5 +25,8 @@ switch ($act) {
     default:
       echo "Your favorite color is neither red, blue, nor green!";
   }
-
+  $res['action'] = $act; 
+  header("content-type: application/json");
+  echo json_encode ($res) ;
+  die() ; 
 ?>
