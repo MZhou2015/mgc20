1a72d2ea5f76324609d2625c3dff6f9862d31088
Michael <ming01zhou@gmail.com>
2020-11-15 13:45:30 -0800

    perl jsn

--- src/perl/getprojson.pl
+++ src/perl/getprojson.pl
@@ -6,36 +6,33 @@ use webquery;
 use URI::Escape;
 use Time::Local;       ##// for convert to timestamp
 webquery::newcookie();
-use DBI;               ##// for convert to timestamp
+use DBI;               ##// import modules for DataBase function
+use JSON;              ## imports encode_json, decode_json, to_json and from_json.
 #############################################################################
 print"Content-type:text\html \n\n";
 print"<html><body>";
 ## my $dbh = DBI->connect('localhost','gecontec','532_47BH5618.zm','gecontec_gs');
 ## $dbh->disconnect;
-my $dbh = DBI->connect("DBI:mysql:gecontec_gs:localhost",'username','532_password');
-my $query  = "SELECT close FROM stk_price_tock WHERE symbol = 'AAPL' and date =  '2020-03-04' ";
-print " $query <br>" ;
-my $sth = $dbh->prepare($query);
-$sth->execute();
-my $result = $sth->fetchrow_hashref();
-print "Value returned: $result->{close}\n </br>";
+my $dbh = DBI->connect("DBI:mysql:gecontec_gs:localhost",'gecontec','532_47BH5618.zm');
 
 my ($from, $to, $fdate, $tdate, $fnear, $tnear, $sub_clear, $sub_insert);
 my ($query, $url, $content);
    $fdate= '21.09.2020  08:30:00';
 my $time = gtimestamp($fdate) ;
 my $now_string = localtime; 
-    
+ print "$time    Date:  $now_string <br>" ;    
 my $ttx  =  date_iso($now_string) ;
 my $time1 = gtimestamp($ttx) ;  
-##########################################
+##############################################
+
 my $paar;
 my $ik = 0 ;
+my @jd ;
 my $filename = '../jsn.json';
 open(FH, '>', $filename) ;
-print("File $filename opened successfully!\n");
+
 print FH "[\n" ;
-my $qq  = "SELECT * FROM gs_product WHERE remark > 100 order  by remark desc";
+my $qq  = "SELECT * FROM gs_product WHERE remark =98 order  by ptcode asc";
  ## print " $key  =>  $dd <br>" ;
 my $cc ; 
 my %sym2;
@@ -46,27 +43,111 @@ my  $sth = $dbh->prepare($qq);
    
    while (my $result = $sth->fetchrow_hashref()) {
           my $act = $result->{ptcode} ;
+       $paar->{code}[$ik] = $act;
+      my $res = getarowdb($act,$dbh) ;  ## load sub routering on line 72
+       if($ik >0 ) { print FH ",\n"; }
+       print FH '{"ptcode":"' ;
+       print FH $result->{ptcode} ;
+       print FH '","model":"' ;
+       print FH $result->{model} ;
+       print FH '","image":"' ;
+       print FH $res->{image} ;
+       print FH '","brief":"' ;
+       print FH $res->{brief} ;
+       print FH '","rprice":"' ;
+       print FH $res->{rprice} ;
+        print FH '",';
+        print FH "\n";
+       print FH '"description":"' ;
+        
+       print FH $res->{description} ;
+       print FH '",';
+         writedescrip($res->{description});
+      print FH ",\n";
+      print FH '"specification":"' ;  ## strart Specifications 
+      print FH $res->{specification} ;
+       print FH '",' ;
+       print FH "\n";            ## End of  Specifications
+       writespecify($res->{specification});
+       print FH '"note":"' ;    ## strart note 
+       print FH $res->{note} ;
+       print FH '"}'; 
       
-       print $result->{remark} ;
-       print FH '{"ptcode":' ;
-       print FH $result->{remark} ;
-       print FH ',"ramark":' ;
-       print FH $act ;
-       print FH "},\n" ;
-     
-       $ik++ ;
+         $ik++ ;
        }
 ############################################################################# 
-## my $jssn = encode_json $paar ;
-## print $js ;
+ my @arr = (1, 2, 'Hello');
+ $arr[3] = "hello" ;
+ print "<br>";
+ my $jssn = encode_json $paar ;
+ print $jssn ;
 #####################################
-
 print FH "\n]" ;
 close(FH);
 # Disconnect from the database.
 $dbh->disconnect();   
 print '</body>
       </html>';
+################################
+sub writedescrip{
+    my $ds_string = shift ;
+    my $string_len = length($ds_string);
+        if ( $string_len > 3) {
+             print FH  "\n";
+           print FH '"descrp":{"here":true,"feature":[';
+           my @ls = split('xx', $ds_string);
+           my $jk = 0;
+              foreach(@ls) {
+                  if($jk >0 ) { print FH ","; }
+                 print FH  '"';
+                 print FH  "$_ ,";
+                print FH  '"';
+                 $jk++;
+                   }
+         print FH " ]}";
+      } else {
+          print FH '"descrp":{"here":false }';
+          print "\n";  
+    }
+}
+################################
+sub writespecify{
+    my $ds_string = shift ;
+    my $string_len = length($ds_string);
+     if ( $string_len > 6) {
+           print FH '"speciff":{"here":true,"feature":[';
+           my @ls = split('xx', $ds_string);
+           my $jk = 0;
+              foreach(@ls) {
+                  if($jk >0 ) { print FH ","; }
+                my @svl =   split('yy', $_);
+                print FH "\n{";
+                print FH '"name":"' ;
+                print FH " $svl[0]";
+                print FH '","sval":"';
+                print FH "$svl[1] " ;
+                 print FH '"}' ;
+                  $jk++;
+                   }
+         print FH " ]},\n";
+      }
+}
+################################
+sub getarowdb{
+  my $cdn = shift ;
+  my $dbk = shift;
+  my $qur  = "SELECT * FROM gs_product_info WHERE ptcode = '$cdn' ";
+  my  $sth = $dbk->prepare($qur) or die "Couldn't prepare the query: $sth->errstr";
+      $sth->execute() or die "Couldn't execute query: $dbh->errstr";;
+  my $val ;
+  while (my $result = $sth->fetchrow_hashref()) {
+        $val =  $result ;
+     }
+  
+   $sth->finish;
+  return $val;
+}
+
 ################################
 sub gtimestamp {
   my $date = shift ;
